978aee0cc0edf8000d4f723cb161204a1a8e1059
Daniel Noworyta <daniel.noworyta@gmail.com>
2020-06-15 13:29:01 +0200

    Removed history from repo

--- .history/src/assets/images/styles/globalStyles_20200425145830.js
+++
@@ -1,7 +0,0 @@
-import {createGlobalStyle} from styled-components
-
-const GlobalStyles = () => {
-  return (  );
-}
- 
-export default GlobalStyles;
--- .history/src/layouts/index_20200425140013.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-const GlobalLayout = () => {
-  return <></>
-}
-
-export default GlobalLayout
