db789c1f6eb2929a26837a1ab3fc1349af4a36d4
Daniel Noworyta <daniel.noworyta@gmail.com>
2020-08-17 15:29:33 +0200

    Styles submit

--- src/pages/confirm.js
+++ src/pages/confirm.js
@@ -1,23 +1,19 @@
 import React from "react"
 import styled from "styled-components"
 
-const ConfirmWrapper = styled.div`
-  width: 100%;
-  max-width: 1536px;
+const ContentWrapper = styled.div`
+  max-width: 800px;
   margin: 0 auto;
 `
 
 const ConfirmPage = () => (
-  <>
-    <ConfirmWrapper>
-      <h1>Jeszcze jedna rzecz...</h1>
-      <p>
-        Dziękuję ci za subskrybowanie mojego newslettera. Teraz musisz jedynie
-        sprawdzić swoją skrzynkę i potwierdzić chęć otrzymywania moich
-        wiadomości.
-      </p>
-    </ConfirmWrapper>
-  </>
+  <ContentWrapper>
+    <h2>Jeszcze jedna rzecz...</h2>
+    <p>
+      Dziękuję ci za subskrybowanie mojego newslettera. Teraz musisz jedynie
+      sprawdzić swoją skrzynkę i potwierdzić chęć otrzymywania moich wiadomości.
+    </p>
+  </ContentWrapper>
 )
 
 export default ConfirmPage
