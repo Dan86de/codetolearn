0d87ff2b63e634893d60f1b88cd9be2bc09ef00e
Daniel Noworyta <daniel.noworyta@gmail.com>
2020-08-17 15:17:15 +0200

    Added styling for thenkyou page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,13 +1,19 @@
 import React from "react"
+import styled from "styled-components"
+
+const ContentWrapper = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+`
 
 const ThanksPage = () => (
-  <>
-    <h1>Dziękuję ci za zubskrybowanie mojego bloga.</h1>
+  <ContentWrapper>
+    <h2>Dziękuję ci za zubskrybowanie mojego bloga.</h2>
     <p>
       Potwierdziłeś chęć otrzymywania moich wiadomości. Możesz spodziewać się
       nowych wiadomości jak tylko umieszczę nowy content na stronie.
     </p>
-  </>
+  </ContentWrapper>
 )
 
 export default ThanksPage
