9d20348dc04310d7ca860496d2704b256f8c0d19
Daniel Noworyta <daniel.noworyta@gmail.com>
2020-09-30 20:29:07 +0200

    Added styles for thankyou page

--- src/pages/dzieki.js
+++ src/pages/dzieki.js
@@ -5,6 +5,9 @@ const ContentWrapper = styled.div`
   max-width: 800px;
   margin: 0 auto;
   padding: 1rem;
+  h2 {
+    line-height: 1.2em;
+  }
   span {
     font-weight: 700;
   }
@@ -12,16 +15,21 @@ const ContentWrapper = styled.div`
 
 const AboutPage = props => (
   <ContentWrapper>
-    <h1>Dzięki za zapisanie się na newsletter!</h1>
+    <h2>Dzięki za zapisanie się na newsletter!</h2>
     <p>
       Newsletter traktuję jako bardziej prywatna forma kontaktu. Możesz
       spokojnie pisać na email <span> daniel@programistafrontend.pl</span>.
       Postaram Ci się odpowiedzieć na wszystkie Twoje pytania i wątpliwości.
-      Chcę pomagać innym, którzy tak jak ja marzą o tym by zostać programistami
-      i pracować w branży IT. Jest to możliwe, niezależnie gdzie się dzisiaj
-      znajdujesz. Potrzeba tylko samozaparcia i cierpliwości. Przy odpowiednio
-      dobranych metodach nauki i jasno określonej drodze też zostaniesz
-      programistą ! Powodzenia!
+      Chcę pomagać innym, którzy tak jak ja marzą o tym, by zostać programistami
+      i pracować w branży IT.
+    </p>
+    <br />
+    <p>
+      Jest to możliwe, niezależnie od tego gdzie się dzisiaj znajdujesz.
+      Potrzeba tylko samozaparcia i cierpliwości. Przy odpowiednio dobranych
+      metodach nauki i jasno określonej drodze też zostaniesz programistą i
+      spełnisz swoje marzenia! Jeżeli mogę Ci jakoś pomóc pisz śmiało!
+      Powodzenia!
     </p>
   </ContentWrapper>
 )
